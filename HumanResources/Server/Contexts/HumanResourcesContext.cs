e42cc9bb55391856fbf67429195055289c218f09
facojosefino_12 <facojosefino_12@live.com.mx>
2021-02-17 00:07:34 -0600

    First Commit

---
+++ HumanResources/Server/Contexts/HumanResourcesContext.cs
@@ -0,0 +1,18 @@
+﻿using HumanResources.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HumanResources.Server.Contexts
+{
+    public class HumanResourcesContext : DbContext
+    {
+        public HumanResourcesContext() { }
+
+        public HumanResourcesContext(DbContextOptions<HumanResourcesContext> options) : base(options){}
+
+        public DbSet<Gender> Gender { get; set; }
+    }
+}
---
+++ HumanResources/Server/Controllers/GenderController.cs
@@ -0,0 +1,60 @@
+﻿using HumanResources.Server.Contexts;
+using HumanResources.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HumanResources.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GenderController : ControllerBase
+    {
+        private readonly HumanResourcesContext context;
+
+        public GenderController(HumanResourcesContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            return context.Gender;
+        }
+
+        [HttpGet("{id}")]
+        public object Get(long id)
+        {
+            return context.Gender.First(gender => gender.Id == id);
+        }
+
+        [HttpPost]
+        public object Post(Gender gender)
+        {
+            context.Add(gender);
+            context.SaveChanges();
+            return gender;
+        }
+
+        [HttpPut]
+        public object Put(Gender gender)
+        {
+            context.Entry(gender).State = EntityState.Modified;
+            context.SaveChanges();
+            return gender;
+        }
+
+        [HttpDelete("{id}")]
+        public object Delete(long id)
+        {
+            Gender g = context.Gender.First(gender => gender.Id == id);
+            context.Remove(g);
+            context.SaveChanges();
+            return g;
+        }
+    }
+}
